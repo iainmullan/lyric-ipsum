1ca912f2060bf62740aa5a6a20a5a07a782dc269
Iain Mullan <iain.mullan@gmail.com>
2015-01-10 17:56:05 +0000

    analytics, twitter, OG etc.

--- deploy/app/views/layout.blade.php
+++ deploy/app/views/layout.blade.php
@@ -3,6 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Lyric Ipsum</title>
+
+	<meta property="fb:admins" content="714655333" />
+	<meta property="og:title" content="Lyric Ipsum | by Iain Mullan" />
+
+	<meta property="og:url" content="http://lyricipsum.com/" />
+	<meta property="og:type" content="website" />
+
 	<style>
 		@import url(//fonts.googleapis.com/css?family=Lato:400,700);
 
@@ -70,6 +77,11 @@
 			max-width: 600px;
 			margin: 0 auto;
 		}
+		.twitter-follow-button {
+			display: block;
+			width: 158px;
+			margin: 20px auto;
+		}
 
 	</style>
 </head>
@@ -87,7 +99,34 @@
 		by <a href="http://iainmullan.com">Iain Mullan</a>
 		at <a href="http://musichackday.org/" target="_blank">Music Hack Day</a> London 2014
 		with the help of <a href="https://developer.musixmatch.com/" target="_blank">musixmatch</a>
+		<br/>
+		<a class="twitter-follow-button"
+          href="https://twitter.com/iainmullan"
+          data-size="small"
+          data-width="178"
+          data-show-count="false"
+          data-lang="en">Follow @iainmullan</a>
+
 	</footer>
 
+    <script type="text/javascript">
+    window.twttr = (function (d, s, id) {
+      var t, js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src= "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+      return window.twttr || (t = { _e: [], ready: function (f) { t._e.push(f) } });
+    }(document, "script", "twitter-wjs"));
+    </script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-323014-41', 'auto');
+	  ga('send', 'pageview');
+	</script>
 </body>
 </html>

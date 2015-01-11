0bc05f9d057b45704e9c2fb183da3b73fa7dda7f
Iain Mullan <iain.mullan@gmail.com>
2015-01-11 14:55:33 +0000

    add viewport meta

--- deploy/app/views/layout.blade.php
+++ deploy/app/views/layout.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title>Lyric Ipsum</title>
 
 	<meta property="fb:admins" content="714655333" />

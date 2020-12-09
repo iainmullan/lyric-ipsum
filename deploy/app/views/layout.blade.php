db93dc5285d169e727799b50e8cdd6ad45731f02
Iain Mullan <iain.mullan@gmail.com>
2020-12-09 10:48:26 +0000

    add fathom analytics

--- deploy/app/views/layout.blade.php
+++ deploy/app/views/layout.blade.php
@@ -84,14 +84,10 @@
 			font-size: 1.3em;
 			margin-bottom: 50px;
 		}
-
-		.twitter-follow-button {
-			display: block;
-			width: 158px;
-			margin: 20px auto;
-		}
-
 	</style>
+	<!-- Fathom - beautiful, simple website analytics -->
+	<script src="https://cdn.usefathom.com/script.js" data-site="IJEJURRT" defer></script>
+	<!-- / Fathom -->
 </head>
 <body>
 
@@ -104,37 +100,9 @@
 	</main>
 
 	<footer>
-		by <a href="http://iainmullan.com">Iain Mullan</a>
+		by <a href="https://iainmullan.com">Iain Mullan</a>
 		at <a href="http://musichackday.org/" target="_blank">Music Hack Day</a> London 2014
-		with the help of <a href="https://developer.musixmatch.com/" target="_blank">musixmatch</a>
-		<br/>
-		<a class="twitter-follow-button"
-          href="https://twitter.com/iainmullan"
-          data-size="small"
-          data-width="178"
-          data-show-count="false"
-          data-lang="en">Follow @iainmullan</a>
-
+		with the help of <a href="https://developer.musixmatch.com/" target="_blank">musixmatch</a>		
 	</footer>
-
-    <script type="text/javascript">
-    window.twttr = (function (d, s, id) {
-      var t, js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src= "https://platform.twitter.com/widgets.js";
-      fjs.parentNode.insertBefore(js, fjs);
-      return window.twttr || (t = { _e: [], ready: function (f) { t._e.push(f) } });
-    }(document, "script", "twitter-wjs"));
-    </script>
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-323014-41', 'auto');
-	  ga('send', 'pageview');
-	</script>
 </body>
 </html>
